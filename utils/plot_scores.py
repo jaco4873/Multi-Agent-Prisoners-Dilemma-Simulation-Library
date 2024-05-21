8ee4af7067f2864deae872f6f518490f63e9dd4b
jaco4873 <124041503+jaco4873@users.noreply.github.com>
2024-05-21 17:17:00 +0200

    update plot_scores function to handle identical agents in matchup

--- utils/plot_scores.py
+++ utils/plot_scores.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-
 def plot_scores(file_path, graphs_dir):
     """
     Plots the scores of different strategies over rounds and saves the plot as a PNG file.
@@ -18,12 +17,25 @@ def plot_scores(file_path, graphs_dir):
 
     data = pd.read_csv(file_path)
 
+    # Check if there are exactly two unique agent names
+    unique_agent_names = data["Agent Name"].unique()
+    if len(unique_agent_names) == 2:
+        # There are exactly two unique names, no need to add identifier
+        data["Unique Agent Name"] = data["Agent Name"]
+    else:
+        # There are not exactly two unique names, add an identifier
+        
+        data['Unique Agent Name'] = data['Agent Name'] + ' ' + data['Agent Identifier'].astype(str)
+
     plt.figure(figsize=(10, 6))
-    for strategy in data["Agent Name"].unique():
-        strategy_data = data[data["Agent Name"] == strategy]
+
+    # Plot the scores for each agent
+    for strategy in data["Unique Agent Name"].unique():
+        strategy_data = data[data["Unique Agent Name"] == strategy]
         plt.plot(strategy_data["Round"], strategy_data["Score"], label=strategy)
 
-    strategy_names = data["Agent Name"].unique()
+    # Create a title for the plot using the first two unique agent names
+    strategy_names = data["Unique Agent Name"].unique()
     title = f"{strategy_names[0]} vs {strategy_names[1]}"
 
     plt.title(title)
